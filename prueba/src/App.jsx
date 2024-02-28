9e8cbfed76e8e72445d43c247ed17eb22be39a42
Alberto Casaus <al100180@gmail.com>
2024-02-28 21:47:16 +0100

    segunda grabacion

--- prueba/src/App.jsx
+++ prueba/src/App.jsx
@@ -1,17 +1,24 @@
 import './style/App.css'
 import React from 'react';
-import Test from './components/Test';
-
+import  ItemCount  from './components/ItemCount'
+import  Navbar  from './components/NavBar'
+import  ItemListContainer  from './components/ItemListContainer'
+import  Item  from './components/Item'
 
 const App = () => {
   return (
-    <div>
-      <Test mensaje = {"Aprendiendo Calculo?"} />
-    </div>
+    <>
+      <Navbar />
+      <ItemCount />
+      <ItemListContainer greeting={"De la tierra media"} />
+     
+    </>
   );
 }
 
 export default App;
 
 
+//<Test mensaje = {"Aprendiendo Calculo?"} />
 
+//<Fetch/>
---
+++ prueba/src/components/CartWidget.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons"
+
+const CartWidget = () => {
+    return (
+        <div>
+            <button className="bg-teal-500 text-white px-4 py-2 rounded flex items-center">
+                <FontAwesomeIcon icon={faShoppingCart} className="mr-2" />
+                <span>0</span>
+            </button>
+            
+        </div>
+    );
+}
+
+export default CartWidget;
